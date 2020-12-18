dfa8a56fdb864d975d8a4d69f48885e150b1d434
wangxiongtao <1220577523@qq.com>
2020-12-18 10:23:10 +0800

    init

---
+++ app/src/main/java/com/dawn/kotlinbasedemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dawn.kotlinbasedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
